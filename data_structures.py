ead1695ff9cd1b52ea2f93abe5ef5fbc2a3a8bf9
joeroser <joeroser@gmail.com>
2025-09-09 20:33:46 +0100

    Started on datastructures

--- data_structures.py
+++ data_structures.py
@@ -1,3 +1,47 @@
 # Important data structures
 
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.size() == 0:
+            return None
+
+        return self.items.pop
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        if self.size() == 0:
+            return None
+
+        return self.items[-1]
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.push(item)
+
+    def pop(self):
+        if self.size() == 0:
+            return None
+
+        return self.items.pop
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        if self.size() == 0:
+            return None
+
+        return self.items[-1]
+    
 
